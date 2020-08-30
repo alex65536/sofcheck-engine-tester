7e278aae9318b1534cf43f44266af482feeb7157
Alexander Kernozhitsky <sh200105@mail.ru>
2020-08-30 16:01:26 +0300

    Move Chess256 stuff into a separate package

---
+++ chess256/Chess256.pas
@@ -0,0 +1,18 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit Chess256;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  BoardHashes, ChessEngines, ChessNotation, ChessRules, ChessStrings, 
+  ChessTime, ChessUtils, EngineProcesses, EngineScores, MoveChains, 
+  MoveConverters, NotationLists, NotationMessaging, NotationTags, 
+  NotationUtils, PGNUtils, UCICommands;
+
+implementation
+
+end.
